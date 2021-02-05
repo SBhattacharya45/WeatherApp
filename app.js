bbc167f6b83375edf3a08ec6f04313541cb2eff7
SBhattacharya45 <umyadeepb27@gmail.com>
2021-02-05 23:30:06 +0530

    finised result page ui and backend

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const axios = require('axios');
+const helper = require("./utility/helper");
 require("dotenv").config();
 
 app.use(express.static("public"));
@@ -17,11 +18,10 @@ app.get("/results", function(req, res){
     .then(response => {
       // handle success
       location = response.data.results[0].geometry;
-      console.log(location);
-      const url = 'https://api.openweathermap.org/data/2.5/onecall?lat='+ location.lat +'&lon='+ location.lng +'&exclude=minutely,hourly,alerts&appid=' + process.env.OPENWEATHER_ID; 
+      const url = 'https://api.openweathermap.org/data/2.5/onecall?lat='+ location.lat +'&lon='+ location.lng +'&exclude=minutely,hourly,alerts&appid=' + process.env.OPENWEATHER_ID + '&units=metric'; 
       axios.get(url)
       .then(weatherData => {
-        console.log(weatherData.data);
+        res.render('results', {data: weatherData.data, location: helper.toTitleCase(query)});
       })
       .catch(error => {
         // handle error
